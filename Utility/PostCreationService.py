b061551960919afa23d6f6482d156c9438be0a4a
OoreFasawe <oore95@gmail.com>
2024-08-10 22:57:05 -0700

    Added text generation from chatgpt

--- Utility/PostCreationService.py
+++ Utility/PostCreationService.py
@@ -2,7 +2,7 @@ import sys
 import os
 sys.path.append(os.path.abspath('/Users/ooreoluwafasawe/Desktop/Coding/Instagram-Autobot'))
 from openai import OpenAI 
-from Classes import Post
+from Classes.Post import Post
 from Details import Application
 import requests
 class PostCreationService(object):
@@ -13,10 +13,27 @@ class PostCreationService(object):
         return cls.instance
 
     def createPost(self):
-        pass
-    
+        previousPosts = self.retrievePreviousPosts()
+        newPost = Post()
+        newPost.text = self.generateText()
+        newPost.image = self.generateImage()
+        return newPost
+
     def savePost(self, post: Post):
         pass
 
-creationService = PostCreationService()
-creationService.createPost()
+    def retrievePreviousPosts():
+        pass
+
+    def generateText(self):
+        client = OpenAI(api_key=Application.keys["api_secret_key"])
+        #TODO(oore): Add better prompt engineering to generate quotes
+        completion = client.chat.completions.create(
+            model="gpt-3.5-turbo", 
+            messages=[{"role": "user", "content": "Give me a short quote enough for an Instagram post"}],
+        ).to_dict()
+        text = completion["choices"][0]["message"]["content"]
+        return text
+    
+    def generateImage(self):
+        pass
