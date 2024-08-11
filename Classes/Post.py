0211082f34f4f761ee911a43cedc7c7e6635451e
OoreFasawe <oore95@gmail.com>
2024-08-11 03:15:58 -0700

    Reset default values for post text and image url to none

--- Classes/Post.py
+++ Classes/Post.py
@@ -1,5 +1,5 @@
 class Post:
-    def __init__(self, imageUrl, text):
+    def __init__(self, imageUrl=None, text=None):
         # Each post should have a picture and some text
         self.imageUrl = imageUrl
         self.text = text
