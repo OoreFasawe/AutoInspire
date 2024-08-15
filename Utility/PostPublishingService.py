0588519c20adf82f5128c605cc6edd37c59dc4a0
tejex <91570985+tejex@users.noreply.github.com>
2024-08-15 16:10:46 -0700

    Very simple version using insta bot

--- Utility/PostPublishingService.py
+++ Utility/PostPublishingService.py
@@ -2,12 +2,22 @@ import sys
 import os
 sys.path.append(os.path.abspath('/Users/ooreoluwafasawe/Desktop/Coding/Instagram-Autobot'))
 from Classes import Post
+from instabot import Bot
+
 class PostPublishingService:
     # singleton design pattern in python
-    def __new__(cls):
+    def __new__(self,cls):
         if not hasattr(cls, 'instance'):
             cls.instance = super(PostPublishingService, cls).__new__(cls)
+            
+        #creating insta bot and logging in here when singleton is created
+        self.bot = Bot()
+        self.bot.login(username='butterman_32', password='ooreAutobot')
+        
         return cls.instance
 
     def publishPost(self, post:Post):
-        pass
+        #Calling upload here
+        self.bot.upload_photo('Path to image here', caption='Your caption goes here')
+        
+        return
