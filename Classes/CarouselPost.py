4b38698aa1f1bb239a0c4d2b8eb30e94b6cd4ec7
OoreFasawe <oore95@gmail.com>
2024-09-05 16:36:21 -0400

    Correction to post superclass initialization

--- Classes/CarouselPost.py
+++ Classes/CarouselPost.py
@@ -7,7 +7,7 @@ from Utility.PostPublishingService import PostPublishingService
 
 class CarouselPost(Post):
     def __init__(self, fileName=None, mediaUrl=None, caption=None, hashtags=None):
-        super.__init__(fileName, mediaUrl, caption, hashtags)
+        super().__init__(fileName, mediaUrl, caption, hashtags)
         self.numberOfPosts = 5
         self.postType = PostTypes.CAROUSEL
 
--- Classes/VideoPost.py
+++ Classes/VideoPost.py
@@ -7,7 +7,7 @@ from Utility.PostPublishingService import PostPublishingService
 
 class VideoPost(Post):
     def __init__(self, fileName=None, mediaUrl=None, caption=None, hashtags=None):
-        super.__init__(fileName, mediaUrl, caption, hashtags)
+        super().__init__(fileName, mediaUrl, caption, hashtags)
         self.numberOfPosts = 1
         self.postType = PostTypes.VIDEO
     
