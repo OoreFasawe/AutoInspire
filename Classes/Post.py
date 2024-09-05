0387c4093e80aa64f0fccc5f50313d361cf117a1
OoreFasawe <oore95@gmail.com>
2024-09-05 16:31:27 -0400

    Added post type as enum to post properties

--- Classes/Post.py
+++ Classes/Post.py
@@ -1,11 +1,20 @@
+from enum import Enum
+class PostTypes(Enum):
+    NONE = 0
+    IMAGE = 1
+    VIDEO = 2
+    CAROUSEL = 3
+
 from abc import abstractmethod
 class Post:
-    def __init__(self, fileName=None, mediaUrls=[], caption=None, hashtags=None):
+    def __init__(self, fileName=None, mediaUrls=[], caption=None, hashtags=None, postType = PostTypes.NONE):
         # Each post should have a picture and some text
         self.fileName = fileName
         self.mediaUrls = mediaUrls
         self.caption = caption
         self.hashtags = hashtags
+        self.postType = postType
+
 
     def __repr__(self):
         return str({"Text": self.text, "Media URLs" : self.mediaUrls})
@@ -14,8 +23,4 @@ class Post:
     def publishPost(self, publishingService):
         pass
 
-from enum import Enum
-class PostTypes(Enum):
-    IMAGE = 1
-    VIDEO = 2
-    CAROUSEL = 3
+
