5b50bd294d69fb13157a15be2a42a230b48f78c4
OoreFasawe <oore95@gmail.com>
2024-09-05 16:34:06 -0400

    Importing modules with absolute path

--- Classes/VideoPost.py
+++ Classes/VideoPost.py
@@ -1,4 +1,10 @@
-from Post import Post, PostTypes
+import sys
+import os
+ABS_PATH = '/Users/ooreoluwafasawe/Desktop/Coding/Instagram-Autobot'
+sys.path.append(os.path.abspath(ABS_PATH))
+from Classes.Post import Post, PostTypes
+from Utility.PostPublishingService import PostPublishingService
+
 class VideoPost(Post):
     def __init__(self, fileName=None, mediaUrl=None, caption=None, hashtags=None):
         super.__init__(fileName, mediaUrl, caption, hashtags)
